7ea82dbd52f69d0a539442acc2f5c9e955f42e64
dsilvestro <silvestro.daniele@gmail.com>
2015-12-12 09:03:34 +0100

    run command line to calc summary t/l from HSP log files

--- pyrate_lib/sum_MCDD.py
+++ pyrate_lib/sum_MCDD.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python 
 import sys
 import numpy as np
 import os.path
 import glob
 from numpy import *
+import argparse
+
+p = argparse.ArgumentParser() #description='<input file>') 
+p.add_argument('-path', type=str,   help='path to log files', default=0, metavar=0)
+p.add_argument('-tag', type=str,   help='path to log files', default="", metavar="")
+p.add_argument('-c', type=int,   help='no. clades to be combined', default=1, metavar=1)
+
+
+args = p.parse_args()
+
+
 
 def calcHPD(data, level) :
 	assert (0 < level < 1)	
@@ -24,18 +36,15 @@ def calcHPD(data, level) :
 
 
 
-infile=raw_input("\n> path to log files: ")
-infile=infile.replace(' ','')
+infile=args.path
 path_dir = infile
 sys.path.append(infile)
 #direct="%s/*gibbs.log" % infile
 
-clades = 8
+clades = args.c
 
-try:
-	model_tag=raw_input("\n> model tag: ")
-except:
-	model_tag=""
+
+model_tag=args.tag
 
 out_list =[]
 for clade in range(clades): out_list.append("c"+str(clade)+model_tag)
@@ -44,8 +53,8 @@ f_nameL = path_dir +"/Gl_summary%s.txt" % (model_tag)
 f_nameM = path_dir +"/Gm_summary%s.txt" % (model_tag)
 outfileL = open(f_nameL,'wb')
 outfileM = open(f_nameM,'wb')
-outfileL.writelines("from	to	mean	median	m	M")
-outfileM.writelines("from	to	mean	median	m	M")
+outfileL.writelines("from	to	mean	median	m	M	support")
+outfileM.writelines("from	to	mean	median	m	M	support")
 
 clade=0
 
@@ -66,28 +75,37 @@ for out in out_list:
 			head = next(open(f)).split()
 			l_indexL = [head.index(i) for i in head if "Gl%s_" % (clade) in i]
 			l_indexM = [head.index(i) for i in head if "Gm%s_" % (clade) in i]
+			
+			k_indexL = [head.index(i) for i in head if "kl%s_" % (clade) in i]
+			k_indexM = [head.index(i) for i in head if "km%s_" % (clade) in i]
+			
+			
 			j=1
 			for i in l_indexL:
 				par = t[:,i]
+				k_par = t[:,k_indexL[j-1]]
 				hpd = np.around(calcHPD(par, .95), decimals=6)
 				#mode = round(get_mode(par),3)
 				mean_par = round(mean(par),6)
 				median_par = round(median(par),6)
+				mean_k= round(median(k_par),6)
 				if f_num==0: 
-					outfileL.writelines("\n%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1]))
+					outfileL.writelines("\n%s\t%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1],mean_k))
 				else: 
-					outfileL.writelines("\n%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1]))
+					outfileL.writelines("\n%s\t%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1],mean_k))
 				j+=1
 			j=1
 			for i in l_indexM:
 				par = t[:,i]
+				k_par = t[:,k_indexM[j-1]]
 				hpd = np.around(calcHPD(par, .95), decimals=6)
 				mean_par = round(mean(par),6)
 				median_par = round(median(par),6)
+				mean_k= round(median(k_par),6)
 				if f_num==0: 
-					outfileM.writelines("\n%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1]))
+					outfileM.writelines("\n%s\t%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1],mean_k))
 				else: 
-					outfileM.writelines("\n%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1]))
+					outfileM.writelines("\n%s\t%s\t%s\t%s\t%s\t%s\t%s" % (j,clade+1, mean_par,median_par,hpd[0],hpd[1],mean_k))
 				j+=1
 			f_num+=1
                 else: pass
