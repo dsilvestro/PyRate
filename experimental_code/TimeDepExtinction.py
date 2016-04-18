6587384f7a2513ecef2d4f867141f72a85956474
Oskar Hagen <oskar.hagen@outlook.com>
2016-04-18 14:34:01 -0300

    Update TimeDepExtinction.py

--- experimental_code/TimeDepExtinction.py
+++ experimental_code/TimeDepExtinction.py
@@ -19,40 +19,28 @@ s = np.random.uniform(1, 25, 10) # random speciation times
 e = np.random.uniform(0, s,  10) # random extinction times
 
 
-#OH# leaving the function below as a reference but changed the calls below to the "new" BDwelik ('w' stands for weibull and 'e' extinction)
-# BD likelihood (constant rate)
-def BDlik (l, m):
-	sp_events = np.ones(len(s))  # define speciation events
-	ex_events = np.zeros(len(e)) # define extinction events
-	ex_events[e>0] = 1           # ex_events = 0 for extant taxa, ex_events=1 for extinct taxa
-	birth_lik = log(l)*sp_events - l*(s-e) # vector likelihoods for each species
-	death_lik = log(m)*ex_events - m*(s-e) #OH# why do you still have the second part for extant taxa here?
-	species_lik = birth_lik + death_lik
-	lik = sum(species_lik)
-	return lik
 
 #OH# Weibull log PDF and CDF functions
-def log_wei_pdf(x,scale,shape):
+def log_wei_pdf(x,W_scale,W_shape):
 	# Log of Weibull pdf
-	log_wei_pdf = log(shape/scale) + (k-1)*log(x/scale) - (x/scale)**shape
+	log_wei_pdf = log(W_shape/W_scale) + (k-1)*log(x/W_scale) - (x/W_scale)**W_shape
 	return log_wei_pdf
 
-def wei_cdf(x,scale,shape):
+def wei_cdf(x,W_scale,W_shape):
 	# Weibull cdf
-	log_wei_cdf = 1 - exp(-(x/scale)**shape)
+	log_wei_cdf = 1 - exp(-(x/W_scale)**W_shape)
 	return wei_cdf
 	
-
 #OH# BDwe likelihood (constant speciation rate and age dependent weibull extinction)
-def BDwelik (l, m, shape, scale):
-	sp_events = np.ones(len(s))  # define speciation events
-	ex_events = np.zeros(len(e)) # define extinction events
+def BDwelik (l, m0, W_shape, W_scale):
+	d = s-e
+	#sp_events = np.ones(len(s))  # define speciation events
+	#ex_events = np.zeros(len(e)) # define extinction events
 	ex_events[e>0] = 1           # ex_events = 0 for extant taxa, ex_events=1 for extinct taxa
-	birth_lik = log(l)*sp_events - l*(s-e) # vector likelihoods for each species
-	#OH# now following the log of PDF for Weibull, when x>=0, which is our case...
-	death_lik = log(m)*ex_events + log_wei_pdf(e,scale,shape) - m*wei_cdf(s-e,scale,shape)
-	species_lik = birth_lik + death_lik
-	lik = sum(species_lik)
+	birth_lik = len(s)*log(l)-sum(l*(d)) # vector likelihoods for each species
+	death_lik_de = sum(log(m0)+log_pdf_Weibull(e[e>0], W_shape, W_scale))
+	dead_lik_wte = -sum(m0*wei_cdf(d,W_scale,W_shape))
+	lik = birth_lik + death_lik_de + death_lik_wte
 	return lik
 
 # prior
@@ -85,28 +73,28 @@ sampling_freq =10
 # init parameters
 lA = 0.5
 mA = 0.1
-shapeA = 0.1 #OH# proposition of parameter, here starting with strong age-dependency, mainly high likelyhood for younger species
-scaleA = 0.1 #OH# proposition of parameter
+W_shapeA = 0.1 #OH# proposition of parameter, here starting with strong age-dependency, mainly high likelyhood for younger species
+W_scaleA = 0.1 #OH# proposition of parameter
 
 while True:
 	# update parameters
 	if np.random.random() >0.5:
 		l, hastings = update_multiplier_proposal(lA)
 		m = mA
-		shape = shapeA
-		scale = scaleA
+		W_shape = W_shapeA
+		W_scale = W_scaleA
 	else:
 		m, hastings = update_multiplier_proposal(mA)
-		shape, hastings = update_multiplier_proposal(shapeA)
-		scale, hastings = update_multiplier_proposal(scaleA)
+		W_shape, hastings = update_multiplier_proposal(W_shapeA)
+		W_scale, hastings = update_multiplier_proposal(W_scaleA)
 		l = lA
 	
 	
 	# calc lik
-	lik = BDwelik(l, m, shape, scale)
+	lik = BDwelik(l, m, W_shape, W_scale)
 	
 	# calc priors
-	prior = prior_gamma(l) + prior_gamma(m) + prior_gamma(shape) + prior_gamma(scale)
+	prior = prior_gamma(l) + prior_gamma(m) + prior_gamma(W_shape) + prior_gamma(W_scale)
 		
 	if iteration ==0:  
 		likA = lik
@@ -121,14 +109,14 @@ while True:
 		priorA = prior
 		lA = l
 		mA = m
-		shapeA = shape
-		scaleA = scale
+		W_shapeA = W_shape
+		W_scaleA = W_scale
 	
 	if iteration % 100 ==0:
-		print likA, priorA, lA, mA, shapeA, scaleA
+		print likA, priorA, lA, mA, W_shapeA, W_scaleA
 		
 	if iteration % sampling_freq ==0:
-		log_state=[iteration,likA+priorA,likA,priorA,lA,mA,shapeA,scaleA]
+		log_state=[iteration,likA+priorA,likA,priorA,lA,mA,W_shapeA,W_scaleA]
 		wlog.writerow(log_state)
 		logfile.flush()
 		
