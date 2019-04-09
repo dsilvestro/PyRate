33b99c2cee98eb28a8b96f159cb7dd1ffb42ac9e
dsilvestro <silvestro.daniele@gmail.com>
2019-04-09 16:27:57 +0200

    plot MTE model testing results

--- experimental_code/plot_MTE_results.R
+++ experimental_code/plot_MTE_results.R
@@ -50,6 +50,45 @@ plot_mte <- function(logfile,color_trait=c(),prior_prob = 0.05){
 	n <- dev.off()
 }
 
+plot_predictor_importance <-function(logfile,color_trait=c(),prior_prob = 0.05,title=""){
+	pdf(file=paste(sub(".log","",logfile,fixed = T),"_BF.pdf",sep=""),width=10, height=10)
+	
+	tbl = read.table(logfile,h=T)
+	indx = grep("I_",colnames(tbl))
+	trait_names = gsub("I_", "", colnames(tbl)[indx])
+
+	if (length(color_trait)==0){
+		color_trait = rep("#08306b",length(trait_names))
+	}
+	
+	print_name= trait_names
+	significance = get_thresholds(prior_prob)
+	predictor_prob = c()
+	colors = c()
+	for (trait_i in 1:length(trait_names)){
+		indx_trait = grep(paste("m_",trait_names[trait_i],sep=""),colnames(tbl))
+		
+		indicator = tbl[,indx[trait_i]]
+		predictor_prob = c(predictor_prob, mean(indicator))
+		if (mean(indicator) > significance[1]){
+			alphaC = 1
+		}else if (mean(indicator) > significance[2]){
+			alphaC = 0.85
+		}else if (mean(indicator) > significance[3]){
+			alphaC = 0.4
+		}else{
+			alphaC = 0.1
+		}
+		colors = c(colors,alphaC )
+	
+	}
+	names(predictor_prob) = trait_names
+	barplot(predictor_prob,ylab="Probability of an effect on extinction",col=alpha(color_trait[1],colors),ylim = c(0,1),main=title)
+	abline(h=significance,lty=2,col=c("#bd0026","#f03b20","#fd8d3c"),lwd=2)
+	n <- dev.off()
+
+}
+
 
 color_trait = c("#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f")
 
