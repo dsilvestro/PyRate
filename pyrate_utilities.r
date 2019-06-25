3c7788fbdfa4850fafb1728de70bdb75692cc4e7
dsilvestro <silvestro.daniele@gmail.com>
2019-06-25 09:50:55 +0200

    added extract.ages.14C function

--- pyrate_utilities.r
+++ pyrate_utilities.r
@@ -50,8 +50,8 @@ extract.ages <- function(file = NULL, replicates = 1, cutoff = NULL, random = TR
 	splist <- unique(dat[,c(1,2)])[order(unique(dat[,c(1,2)][,1])),]
 
 
-	if (any(is.element(splist$Species[splist$Status == "extant"], splist$Species[splist$Status == "extinct"]))){
-		print(intersect(splist$Species[splist$Status == "extant"], splist$Species[splist$Status == "extinct"]))
+	if (any(is.element(splist$Species[tolower(splist$Status) == "extant"], splist$Species[tolower(splist$Status) == "extinct"]))){
+		print(intersect(splist$Species[tolower(splist$Status) == "extant"], splist$Species[tolower(splist$Status) == "extinct"]))
 		stop("at least one species is listed as both extinct and extant\n")
 	}
 
@@ -239,3 +239,89 @@ extract.ages.tbl <- function(file = NULL,sep="\t", extant_species = c(), replica
 	extract.ages(file=output_file,replicates = replicates, cutoff = cutoff, random = random)
 }
 
+extract.ages.14C <- function(file,outname = "_PyRate"){
+	file = "/Users/danielesilvestro/Desktop/UCLA_projects/Erik_PLOSone/sample_results_calib.txt"
+	dat <- read.table(file, header=T, stringsAsFactors=F, row.names=NULL, sep="\t", strip.white=T)
+	fname <- no.extension(basename(file))
+	outfile <- paste(dirname(file), "/", fname, outname, ".py", sep="")
+
+	dat[,1] <- gsub("[[:blank:]]{1,}","_", dat1[,1])
+
+
+	colnames(dat)[1] = "Lineage"
+	colnames(dat)[2] = "Status"
+	dat$new_age <- "NA"
+	splist <- unique(dat[,c(1,2)])[order(unique(dat[,c(1,2)][,1])),]
+
+
+	if (any(is.element(splist$Species[tolower(splist$Status) == "extant"], splist$Species[tolower(splist$Status) == "extinct"]))){
+		print(intersect(splist$Species[tolower(splist$Status) == "extant"], splist$Species[tolower(splist$Status) == "extinct"]))
+		stop("at least one species is listed as both extinct and extant\n")
+	}
+
+	cat("#!/usr/bin/env python", "from numpy import * ", "",  file=outfile, sep="\n")
+	
+	replicates = dim(dat)[2]-2
+	for (j in 1:replicates){
+		times <- list()
+		cat ("\nreplicate", j)
+	
+		dat$new_age = dat[,j+2]
+
+		dat2 <- subset(dat, select=c("Lineage","new_age"))
+		taxa <- sort(unique(dat2$Lineage))
+
+		for (n in 1:length(taxa)){
+			times[[n]] <- dat2$new_age[dat2$Lineage == taxa[n]]
+			if (toupper(splist$Status[splist$Lineage == taxa[n]]) == toupper("extant")){
+				times[[n]] <- append(times[[n]], "0", after=length(times[[n]]))
+			}
+		}
+
+		dat3 <- matrix(data=NA, nrow=length(times), ncol=max(sapply(times, length)))
+		rownames(dat3) <- taxa
+
+		for (p in 1:length(times)){
+			dat3[p,1:length(times[[p]])] <- times[[p]]
+		}
+
+		cat(noquote(sprintf("\ndata_%s=[", j)), file=outfile, append=TRUE)
+
+		for (n in 1:(length(taxa)-1)){
+			rec <- paste(dat3[n,!is.na(dat3[n,])], collapse=",")
+			cat(noquote(sprintf("array([%s.]),", rec)), file=outfile, append=TRUE, sep="\n")
+		}
+
+		n <- n+1
+		rec <- paste(dat3[n,!is.na(dat3[n,])], collapse=",")
+		cat(noquote(sprintf("array([%s.])", rec)), file=outfile, append=TRUE, sep="\n")
+
+		cat("]", "", file=outfile, append=TRUE, sep="\n")
+	}
+
+
+	data_sets <- ""
+	names <- ""
+
+	if (replicates > 1){
+		for (j in 1:(replicates-1)) {
+			data_sets <- paste(data_sets, noquote(sprintf("data_%s,", j)))
+			names <- paste(names, noquote(sprintf(" '%s_%s',", fname,j)))
+			}
+
+		data_sets <- paste(data_sets, noquote(sprintf("data_%s", j+1)))
+		names <- paste(names, noquote(sprintf(" '%s_%s',", fname,j+1)))
+	} else {
+		data_sets <- "data_1"
+		names <- noquote(sprintf(" '%s_1'", fname))	
+	}
+
+	cat(noquote(sprintf("d=[%s]", data_sets)), noquote(sprintf("names=[%s]", names)), "def get_data(i): return d[i]", "def get_out_name(i): return  names[i]", file=outfile, append=TRUE, sep="\n")
+
+
+	tax_names <- paste(taxa, collapse="','")
+	cat(noquote(sprintf("taxa_names=['%s']", tax_names)), "def get_taxa_names(): return taxa_names", file=outfile, append=TRUE, sep="\n")
+
+  	cat("\n\nPyRate input file was saved in: ", sprintf("%s", outfile), "\n\n")
+
+}
