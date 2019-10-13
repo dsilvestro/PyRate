4ca518af01aae6cfaab7bbc93778c11ff2db19aa
dsilvestro <silvestro.daniele@gmail.com>
2019-10-13 09:34:02 +0200

    Update plot_root_estimates.R

--- experimental_code/plot_root_estimates.R
+++ experimental_code/plot_root_estimates.R
@@ -1,6 +1,7 @@
 f="/Users/danielesilvestro/Software/PyRate_github/experimental_code/rootest_q_5_1_epsilon_0.5_fZero_0.25_thr_11.log"
 f="/Users/danielesilvestro/Dropbox (Personal)/AngiospermOrigin/fossil_data/rootest_q_8.52_1_epsilon_0.5_fZero_0.1_thr_11.log"
-f="/Users/danielesilvestro/Dropbox (Personal)/AngiospermOrigin/fossil_data/rootest_q_8.52_1_epsilon_0.5_fZero_0.1_thr_11_incr_q.log"
+f="/Users/danielesilvestro/Software/PyRate_github/experimental_code/test_q_8.52_1_epsilon_0.5_fZero_0.1_thr_11.log_estQslope1.log"
+f="/Users/danielesilvestro/Software/PyRate_github/experimental_code/test_q_8.52_1_epsilon_0.5_fZero_0.1_thr_11_incr_q_estQslope1.log"
 
 library(scales)
 library(HDInterval)
@@ -8,19 +9,22 @@ tbl = read.table(f,h=T)
 par(mfrow=c(1,3))
 indxWFOSS = which(tbl$Nfossils>0)
 tbl = tbl[indxWFOSS,]
-plot( tbl$root_true,tbl$root_est, xlim = c(0,200), ylim = c(0,200),pch = 19,col=alpha("black",0.2),type="n")
+plot( tbl$root_true,tbl$root_est, xlim = c(0,200), ylim = c(0,200),pch = 19,col=alpha("black",0.2),type="p")
 points( tbl$root_true,tbl$root_obs, col=alpha("red",0.2),pch = 19)
 abline(0,1,lty=2,col="gray")
 
 segments(x0 =tbl$root_true, x1=tbl$root_true, y0=tbl$root_m2, y1=tbl$root_M2,lwd=4,col=alpha("black",0.2))
 segments(x0 =tbl$root_true, x1=tbl$root_true, y0=tbl$root_m4, y1=tbl$root_M4,lwd=2,col=alpha("black",0.2))
-segments(x0 =tbl$root_true, x1=tbl$root_true, y0=tbl$root_mth, y1=tbl$root_Mth,lty=1,col=alpha("black",0.2))
+segments(x0 =tbl$root_true, x1=tbl$root_true, y0=tbl$root_mth, y1=tbl$root_Mth,lty=1,col=alpha("red",0.2))
 
-
+hist(tbl$root_M2-tbl$root_m2)
 #plot( (tbl$root-tbl$root_true)/tbl$root_true, tbl$root-tbl$root_obs)
 #plot( (tbl$root-tbl$root_true)/tbl$root_true, tbl$mu0_true)
 #plot( (tbl$root-tbl$root_true)/tbl$root_true, tbl$q_med_true)
-#plot( (tbl$root-tbl$root_true)/tbl$root_true, tbl$root)
+plot( (tbl$root_obs-tbl$root_true), tbl$delta_lik)
+
+
+
 
 
 hist( (tbl$root_obs-tbl$root_true)/tbl$root_true)
@@ -50,6 +54,6 @@ summary(tbl$delta_lik)
 plot(tbl$delta_lik, tbl$Nobs)
 library(HDInterval)
 hdi(tbl$delta_lik)
-hdi(tbl$delta_lik,0.99)
+hdi(tbl$delta_lik,0.95)
 mean(tbl$delta_lik)
 median(tbl$delta_lik)
