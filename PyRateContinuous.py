fix equilibrium calculation for dispersal dependent extinction

PyRateContinuous.py
@@ -123,10 +123,10 @@ else:
 out_model = ["const","exp","lin"]
 
 #print len(ts),len(te[te>0]),sum(ts-te)
+pad_s_times = np.concatenate( (s_times + np.repeat(0.01, len(s_times)), s_times + np.repeat(-0.01, len(s_times))) )
+pad_s_times = pad_s_times[pad_s_times >= 0]
 if args.DD is True:
 	head_cov_file = ["","DD"]
-	pad_s_times = np.concatenate( (s_times + np.repeat(0.01, len(s_times)), s_times + np.repeat(-0.01, len(s_times))) )
-	pad_s_times = pad_s_times[pad_s_times >= 0]
 	ts_te_vec = np.sort( np.concatenate((ts,te,pad_s_times)) )[::-1]
 	Dtraj = getDT(ts_te_vec,ts,te) + np.zeros(len(ts_te_vec))
 	times_of_T_change =  ts_te_vec
@@ -139,6 +139,17 @@ else:
 	head_cov_file = next(open(cov_file)).split()
 	times_of_T_change= tempfile[:,0]*args.rescale # array of times of Temp change
 	Temp_values=       tempfile[:,1] # array of Temp values at times_of_T_change
+	trim_temp = times_of_T_change <= max(ts)
+	times_of_T_change = times_of_T_change[trim_temp]
+	Temp_values = Temp_values[trim_temp]
+	times_of_T_change_indexes = np.concatenate((np.zeros(len(times_of_T_change)),np.zeros(len(pad_s_times))+1))
+	times_incl_s_times = np.concatenate((times_of_T_change,pad_s_times))
+	idx = np.argsort(times_incl_s_times)
+	times_incl_s_times_ord = times_incl_s_times[idx]
+	times_of_T_change_indexes_ord = times_of_T_change_indexes[idx]
+	times_incl_s_times_ord = times_incl_s_times[idx]
+	Temp_values = get_VarValue_at_time(times_incl_s_times_ord,Temp_values,times_of_T_change_indexes_ord,times_of_T_change,max(times_incl_s_times))
+	times_of_T_change = times_incl_s_times_ord
 
 # Temp_values= (Temp_values-Temp_values[0]) # so l0 and m0 are rates at the present
 if rescale_factor > 0: Temp_values = Temp_values*rescale_factor
@@ -564,7 +575,10 @@ for iteration in range(mcmc_gen * len(scal_fac_TI)):
 		# shift_ind = [0,0,0,1,1,2,2,2,2,...N], where 0 is index of oldest bin, N of the most recent
 		shift_ind_temp_CURVE =Itemp.astype(int)
 		modified_Temp_at_events[shift_ind_temp_CURVE==0] = modified_Temp_at_events[ (shift_ind_temp_CURVE==1).nonzero()[0][0]  ]
-
+#	l0 = np.repeat(1.1, n_time_bins)
+#	m0 = np.repeat(0.9, n_time_bins)
+#	Garray[0] = np.repeat(0.2, n_time_bins)
+#	Garray[1] = np.repeat(0.2, n_time_bins)
 	
 	if args_mSpEx[0]==0: 
 		l_at_events=trasfMultipleRateTemp(l0, Garray[0],modified_Temp_at_events,shift_ind)

PyRateDES2.py
@@ -703,7 +703,7 @@ def div_dep_ext_dt(div, t, d12, d21, mu1, mu2, k_d1, k_d2, covar_mu1, covar_mu2)
 	dS[3] = d21 * div2 * lim_d1 # Gain area 1
 	dS[4] = d12 * div1 * lim_d2 # Gain area 2
 	mu1 = mu1 + covar_mu1 * dS[3] / (div13 + 1.) #mu1 * np.exp(covar_mu1 * dS[3] / (div13 + 1.))
-	mu2 = mu2 * covar_mu2 * dS[4] / (div23 + 1.) #mu2 * np.exp(covar_mu2 * dS[4] / (div23 + 1.))
+	mu2 = mu2 + covar_mu2 * dS[4] / (div23 + 1.) #mu2 * np.exp(covar_mu2 * dS[4] / (div23 + 1.))
 	dS[0] = -mu1 * div1 + mu2 * div3 - dS[4]
 	dS[1] = -mu2 * div2 + mu1 * div3 - dS[3]
 	dS[2] = -(mu1 + mu2) * div3 + dS[3] + dS[4]
@@ -841,8 +841,8 @@ def calc_diff_equil_two_areas(div):
 		gain1 = dis[0] * div2 * lim_d2
 		gain2 = dis[1] * div1 * lim_d1
 		if argsDdE: # Dispersal dependent extinction
-			mu1 = ext[0] * np.exp(covar_par[2] * gain1/(div13 + 1.))
-			mu2 = ext[0] * np.exp(covar_par[3] * gain2/(div23 + 1.))
+			mu1 = ext[0] + covar_par[2] * gain1 / (div13 + 1.)
+			mu2 = ext[1] + covar_par[3] * gain2 / (div23 + 1.)
 		else: # Diversity dependent extinction
 			lim_e1 = max(1e-10, 1 - div13/k_e[0]) # Increases extinction in area 2
 			lim_e2 = max(1e-10, 1 - div23/k_e[1]) # Increases extinction in area 1
@@ -862,7 +862,7 @@ def calc_diff_equil_one_area(div):
 		lim_d = max(0, 1 - div_both/k_d)  # Limit dispersal into focal area
 		gain = dis * div[0] * lim_d
 		if argsDdE: # Dispersal dependent extinction
-			mu = ext * np.exp(covar_par_equil * gain/(div_both + 0.00001))
+			mu = ext + covar_par_equil * gain / (div_both + 1.)
 		else: # Diversity dependent extinction
 			lim_e = max(1e-10, 1 - div_both/k_e)
 			mu = ext/lim_e