5edd234eff0fa1eef32d2985b63a245f9532ff55
Shelley Wang <seashellsw@gmail.com>
2024-06-05 17:01:01 -0400

    removing the .R tutorial follow through

--- tutorials/pyrate_tutorial_1_followthrough.R
+++
@@ -1,6 +0,0 @@
-source("pyrate_utilities.r")
-
-extant_dogs = c("Canis rufus","Canis lupus","Canis aureus","Canis latrans","Canis mesomelas","Canis anthus","Pseudalopex gymnocercus","Canis adustus","Canis familiaris")
-print(extant_dogs)
-extract.ages.pbdb(file = "example_files/Canis_example/Canis_pbdb_data.csv", extant_species = extant_dogs)
-# This should've created two files 
