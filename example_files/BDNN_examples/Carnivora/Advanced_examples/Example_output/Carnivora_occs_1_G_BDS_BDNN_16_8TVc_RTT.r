d371fa32a622300765569510b01880741ed15d00
thauffe <torsten.hauffe@gmail.com>
2024-08-23 09:57:07 +0200

    remove r scripts

--- example_files/BDNN_examples/Carnivora/Advanced_examples/Example_output/Carnivora_occs_1_G_BDS_BDNN_16_8TVc_RTT.r
+++
@@ -1,5 +0,0 @@
-pdf(file='./example_files/BDNN_examples/Carnivora/pyrate_mcmc_logs/Carnivora_occs_1_G_BDS_BDNN_16_8TVc_RTT.pdf', width = 9, height = 0, useDingbats = FALSE)
-
-layout(matrix(1:0, ncol = 2, nrow = 0, byrow = TRUE))
-par(las = 1, mar = c(4.5, 4.5, 0.5, 0.5))
-dev.off()
